a810b813911f277a46fbeb0bace2580a48fd9f42
takami <takami.rodriguez@gmail.com>
2021-01-07 21:32:58 -0600

    -Changed columns attributes for products -Installed necessary dependency

---
+++ database/migrations/2021_01_08_032502_change_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Changing columns for demostration purpose
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('price')->nullable()->change();
+            $table->string('description')->nullable()->change();
+            $table->string('image_url')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
